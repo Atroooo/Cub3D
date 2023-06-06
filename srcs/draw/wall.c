d05c8307bec5ce331b3497f89aa4416df8999d19
Gerardin Clement <gclement@z1r6p5.42lyon.fr>
2023-06-06 10:31:47 +0200

    Revert "Affichage un peu meilleur"
    This reverts commit 7c02a2e9fb3b26eb602c1defaf947b2a3485d6a1.

--- srcs/draw/wall.c
+++ srcs/draw/wall.c
@@ -3,35 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   wall.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gclement <gclement@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lcompieg <lcompieg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 14:17:15 by gclement          #+#    #+#             */
-/*   Updated: 2023/06/05 17:40:43 by gclement         ###   ########.fr       */
+/*   Updated: 2023/06/05 16:39:26 by lcompieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void	draw_wall(float distance, t_img *img, float *P_x)
+void	draw_wall(float distance, t_img *img, float *P_x, float y)
 {
-	int		x;
-	float	y;
+	int	x;
 
 	x = *P_x;
-	y = 0;
-	while (y < (WALL_H / distance) / 4)
-	{
-		my_mlx_pixel_put(img, x, y, 0x75cce6);
-		y += 1;
-	}
 	while (y < (WALL_H / distance))
 	{
 		my_mlx_pixel_put(img, x, y, 0x757575);
-		y += 1;
-	}
-	while (y < WIN_HEIGHT)
-	{
-		my_mlx_pixel_put(img, x, y, 0xe6bd75);
-		y += 1;
+		y += 0.1;
 	}
 }
