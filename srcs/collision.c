d590f25f08d3adb721fa29da0d04391a35e5e191
Lucas Compiegne <lcompieg@z2r7p8.42lyon.fr>
2023-06-08 14:53:49 +0200

    collisions almost ok

--- srcs/collision.c
+++ srcs/collision.c
@@ -3,21 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   collision.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lcompieg <lcompieg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 11:40:23 by gclement          #+#    #+#             */
-/*   Updated: 2023/06/07 22:03:39 by marvin           ###   ########.fr       */
+/*   Updated: 2023/06/08 13:58:32 by lcompieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
+//Reflechir a une solution pour les distances bas et droite : le "pixel" du 1 est plus loin que les 2 autres murs
 static float	distance_wall(float x, float y, t_data *data)
 {
 	float	dist;
 	int		x_t;
 	int		y_t;
-	
+
 	dist = 0.0;
 	x_t = x / TILE_SIZE;
 	y_t = y / TILE_SIZE;
@@ -28,22 +29,30 @@ static float	distance_wall(float x, float y, t_data *data)
 		y += (y - data->p_pos_y);
 		x_t = x / TILE_SIZE;
 		y_t = y / TILE_SIZE;
-		dist += 0.1;
+		dist += 0.05;
 	}
+	// if (y_t == data->map_data.map_height - 1)
+	// 	return (dist / 3.0); //Solution ok pour une map carre mais pas pour une map avec forme speciale
 	return (dist);
 }
 
 int	collision(int x, int y, t_data *data, char c)
 {
-	(void) x;
-	(void) y;
-	(void) c;
-	// if (c == 'N' || c == 'S')
-	// {
-		if (distance_wall(data->p_pos_x + data->p_delta_x, \
-			data->p_pos_y + data->p_delta_y, data) <= 0.15)
+	float	dist;
+
+	dist = distance_wall(x, y, data);
+	printf("dist = %f\n", dist);
+	if (c == 'N')
+		if (dist <= 0.05)
 			return (0);
-		return (1);
-	// }
-	// return (1);
+	if (c == 'S')
+		if (dist <= 0.05)
+			return (0);
+	if (c == 'W')
+		if (dist <= 0.05)
+			return (0);
+	if (c == 'E')
+		if (dist <= 0.05)
+			return (0);
+	return (1);
 }
