4966937c90aa2554aeb41968c13e407235ee612a
Lucas Compiegne <lcompieg@z1r3p5.42lyon.fr>
2023-06-28 14:18:11 +0200

    fix parsing

--- Makefile
+++ Makefile
@@ -17,6 +17,7 @@ SRC			 			= main.c \
 						  parsing/get_map.c \
 						  parsing/get_map_utils.c \
 						  parsing/get_map_utils_bis.c \
+						  parsing/get_map_utils_th.c \
 						  parsing/error_message_parsing.c \
 						  init/init_mlx_create_win.c \
 						  init/key_hook.c \
--- srcs/parsing/get_map_utils.c
+++ srcs/parsing/get_map_utils.c
@@ -3,53 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   get_map_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lcompieg <lcompieg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 12:55:27 by lcompieg          #+#    #+#             */
-/*   Updated: 2023/06/27 18:35:45 by marvin           ###   ########.fr       */
+/*   Updated: 2023/06/28 14:13:50 by lcompieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
-
-int	check_char_value(t_data *data, int i, int j)
-{
-	char	**map;
-
-	map = data->map_data.map;
-	if (map[i][j] == '0' || map[i][j] == 'N' || \
-	map[i][j] == 'S' || map[i][j] == 'E' || \
-	map[i][j] == 'W' || map[i][j] == 'O' || \
-	map[i][j] == 'D')
-		return (1);
-	return (0);
-}
-
-int	check_bord_char(t_data *data)
-{
-	char	**map;
-	int		i;
-	int		j;
-
-	map = data->map_data.map;
-	j = 0;
-	if (!map[0] || !map[0][0])
-		return (0);
-	while (map[0][j])
-	{
-		if (map[0][j] && check_char_value(data, 0, j))
-			return (0);
-		j++;
-	}
-	i = 0;
-	while (map[i])
-	{
-		if (map[i][0] && check_char_value(data, i, 0))
-			return (0);
-		i++;
-	}
-	return (1);
-}
 
 static int	check_char(char c)
 {
@@ -65,20 +26,11 @@ static int	check_char(char c)
 	return (0);
 }
 
-static int	check_next_char(char **map, int i, int j, t_map_info map_info)
+static int	check_next_char(char **map, int i, int j)
 {
 	if (map[i][j + 1] == '\0' && (map[i][j] == '0' || map[i][j] == 'N' \
 	|| map[i][j] == 'S' || map[i][j] == 'E' || map[i][j] == 'W' || \
 	map[i][j] == 'D' || map[i][j] == 'O'))
-		return (0);
-	if (map[map_info.map_height - 1][j] && \
-		(map[map_info.map_height - 1][j] == '0' || \
-		map[map_info.map_height - 1][j] == 'N' || \
-		map[map_info.map_height - 1][j] == 'E' || \
-		map[map_info.map_height - 1][j] == 'W' || \
-		map[map_info.map_height - 1][j] == 'S' || \
-		map[map_info.map_height - 1][j] == 'D' || \
-		map[map_info.map_height - 1][j] == 'O'))
 		return (0);
 	if ((map[i][j - 1] && map[i][j - 1] == ' ') || \
 		(map[i][j + 1] && map[i][j + 1] == ' ') || \
@@ -107,7 +59,7 @@ void	check_map(t_data *data)
 			|| map[i][j] == 'S' || map[i][j] == 'E' || map[i][j] == 'W' || \
 			map[i][j] == 'D' || map[i][j] == 'O'))
 			{
-				if (map[i][j] && !check_next_char(map, i, j, data->map_data))
+				if (map[i][j] && !check_next_char(map, i, j))
 				{
 					printf("Error\nMap is not closed.\n");
 					free_parsing(data);
